e7534f46900568f3675c48ef9ad478032064e1f9
spideyk21 <spideyk21@gmail.com>
2016-04-12 13:56:26 -0400

    added button press
    added button press to trigger camera

--- pitrailcam_camera-test.py
+++ pitrailcam_camera-test.py
@@ -16,6 +16,18 @@
 
 # Import required Python libraries
 import RPi.GPIO as GPIO
+import time
+import os
+
+# Use BCM GPIO references instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
+GPIO_BUTTON = 17
+
+# Set GPIO pins (input/output)
+GPIO.setup(GPIO_BUTTON,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 
 #pic_loc = "/home/pi/webcam/"
 pic_loc = "/media/LinHES_201502/"
@@ -23,5 +35,12 @@ DATE = time.strftime("%Y%m%d_%H%M%S")
 
 print pic_loc
 
-# Take Picture with options
-os.system ("raspistill -n -vf -hf -o " + pic_loc + DATE + ".jpg") #sh
+try:  
+ while True:
+    if GPIO.input(switch_pin) == False:
+        # Take Picture with options
+		os.system ("raspistill -n -vf -hf -o " + pic_loc + DATE + ".jpg") #sh command
+		time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
