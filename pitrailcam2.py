062fe43e895013bc2ed4f42cc532d8fcdf646fef
Jason <spideyk21@gmail.com>
2016-06-12 08:47:35 -0400

    wip

---
+++ pitrailcam2.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#title           : pitrailcam2.py
+#description     : Raspberry Pi based Trailcam
+#author          : spideyk21
+#date            : MM/DD/YYYY
+#version         : X.X
+#usage           : 
+#refrence        : http://www.raspberrypi.org/documentation/usage/camera/python/README.md
+#				   http://picamera.readthedocs.io/
+#				   https://www.raspberrypi.org/documentation/raspbian/applications/camera.md
+#				   http://www.afraidofsunlight.co.uk/weather/trailcam_info.html
+#				   http://richardhayler.blogspot.co.uk/2016/05/naturebytes-lite-bare-bones-build-for.html
+#
+#python_version  : 3.0 (sudo apt-get install python3-picamera)
+#notes			 : written using gpiozero (https://gpiozero.readthedocs.org/)
+#==============================================================================
+
+# Import required Python libraries
+from gpiozero import MotionSensor, Button, LED
+from picamera import PiCamera
+import time
+
+pir = MotionSensor(17)
+camera = PiCamera()
+
+#dip_cmode = Button(#)
+#dip_pmode = Button(#)
+#dip_time1 = Button(#)
+#dip_time2 = Button(#)
+#dip_time3 = Button(#)
+#dip_button6 = Button(#)
+#dip_button7 = Button(#)
+#dip_button8 = Button(#)
+
+
+while True:
+	pir.wait_for_motion()
+	print("Motion Detected")
+	filename = time.strftime("%Y%m%d_%H%M%S")
+	camera.resolution = (1024,768)
+	camera.capture('/home/pi/Pictures/' + filename + '.jpg')
+	time.sleep(2)
--- pitrailcam_pir-test2.py
+++ pitrailcam_pir-test2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #title           : pitrailcam_pir-test2.py
 #description     : check PIR sensor input
 #author          : spideyk21
@@ -7,19 +7,33 @@
 #usage           : 
 #notes           : http://www.raspberrypi-spy.co.uk/2013/01/cheap-pir-sensors-and-the-raspberry-pi-part-1/
 #
-#python_version  : 3.0
-#notes			 : uses gpiozero (https://gpiozero.readthedocs.org/)
+#python_version  : 3
+#notes		 : uses gpiozero (https://gpiozero.readthedocs.org/)
 #==============================================================================
 
 # Import required Python libraries
 from gpiozero import LED, MotionSensor
 from signal import pause
+import time
+import picamera
 
 pir = MotionSensor(17) #set pir pin number
-led = LED(#) #set led pin number
+#led = LED(#) #set led pin number
+
+camera = picamera.PiCamera()
 
 while True:
 	pir.wait_for_motion()
-	print("Motion detected!")
+#	print("Motion detected!")
+#	ts = time.strftime("%Y%m%d_%H%M%S")
+#	camera.capture('/home/pi/Pictures/' + ts + '.jpg')
+#	camera.close()
+
+	with picamera.PiCamera() as camera:
+		camera.capture('foo.jpg')
+
+	time.sleep(5)
+	
 
 pause()
+
